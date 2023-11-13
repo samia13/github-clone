98a74a34eaac6c8d475fb8e96ebabfeafcf44e8e
Mahi samia <mahisamia13@gmail.com>
2023-11-13 14:27:50 +0100

    Tailwind theme colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#0c1015",
+        "purple-color": "#713fc8",
+        "light-purple-color": "#7c72ff",
+        "green-color": "#3fb950",
+        "blue-color": "#33b3ae",
+        "pink-color": "#33b3ae",
+        "p-color": "#7d8590",
+      },
+    },
   },
   plugins: [],
 };
